e10f6a73e2749c2e0dce894b711cda478bfe0fb6
MehmetAzizYirik <mehmetazizyirik@outlook.com>
2019-05-28 13:32:49 +0200

    License

--- src/main/java/Orbits/PermutationGroupFunctions.java
+++ src/main/java/Orbits/PermutationGroupFunctions.java
@@ -1,3 +1,39 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 Mehmet Aziz Yirik
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/**
+ * This class is the re-implementation of MOLGEN project. For more details;
+ * please check Mathematical Chemistry and Cheminformatics book[1]
+ * 
+ * [1] Kerber, A., Laue, R., Meringer, M., Rücker, C. and Schymanski, E., 2013.
+ * Mathematical chemistry and chemoinformatics: structure generation, elucidation
+ * and quantitative structure-property relationships. Walter de Gruyter.
+ * 
+ * @author Mehmet Aziz Yirik
+ */
+
 package Orbits;
 
 import java.io.IOException;
