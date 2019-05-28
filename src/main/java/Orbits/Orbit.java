e9fb21c8d54248d0690f0daa56615265ada9bdc1
MehmetAzizYirik <mehmetazizyirik@outlook.com>
2019-05-28 14:01:17 +0200

    License

--- src/main/java/Orbits/Orbit.java
+++ src/main/java/Orbits/Orbit.java
@@ -1,3 +1,41 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Mehmet Aziz Yirik
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/**
+ * This class is the re-implementation of group algorithms from C.A.G.E.S. book.[1] 
+ * The functions are from Chapter 6: Groups and Symmetry. The source codes, given with 
+ * the book, have been converted from C to Java.
+ * 
+ * [1] Kreher, D.L. and Stinson, D.R., 1998. Combinatorial algorithms: generation,
+ * enumeration, and search (Vol. 7). CRC press.
+ * 
+ * @author Mehmet Aziz Yirik
+ */
+
+
+
 package Orbits;
 
 import java.util.HashSet;
