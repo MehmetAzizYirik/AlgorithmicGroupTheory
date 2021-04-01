06b0793994d5685095ebbe296b803da31ab37fb6
Valentyn Kolesnikov <javadev75@gmail.com>
2021-04-01 17:04:54 +0300

    Added unit test for C5H9N3 generation.

---
+++ src/test/java/AlgorithmicGroupTheory/MorgenTest.java
@@ -0,0 +1,21 @@
+package AlgorithmicGroupTheory;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class MorgenTest {
+
+    @Test
+    public void main() throws Exception {
+        String[] argument = {"-f", "C5H9N3", "-v", "-d", "result1"};
+//        String[] argument2 = {"-f", "C2H5NO2", "-v", "-d", "result2"};
+//        String[] argument3 = {"-f", "C4H7NO3", "-v", "-d", "result3"};
+//        String[] argument4 = {"-f", "C5H9N3", "-v", "-d", "result4"};
+//        List<String[]> arguments = Arrays.asList(argument1, /*argument2,*/ argument3, argument4);
+        System.out.println("First call");
+        MORGEN.main(argument);
+        System.out.println("Second call");
+        MORGEN.main(argument);
+    }
+}
